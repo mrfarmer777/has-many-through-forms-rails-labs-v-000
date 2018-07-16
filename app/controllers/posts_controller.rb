b5272af8cea977e1b422611e797d18d469a0c2b8
Matt Farmer <mrfarmer777@gmail.com>
2018-07-16 16:42:20 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,20 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    
   end
 
   def create
     post = Post.create(post_params)
     redirect_to post
+  end
+
+  #custom setter for nested category creation
+  def categories_attributes=(category_attributes)
+    category_attributes.value.each do |attribute|
+      category=Category.find_or_create_by(attribute)
+      self.categories<<category
+    end
   end
 
   private
