7713b111e4b3ed7694af39cf7ff9042609135f77
Matt Farmer <mrfarmer777@gmail.com>
2018-07-17 04:04:09 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,13 @@ class CommentsController < ApplicationController
   def create
     #raise params.inspect
     comment = Comment.create(comment_params)
-    
+
     redirect_to post_path(comment.post)
   end
 
   def user_attributes=(user_hash)
     if !user_hash[:username].blank?
-      raise user_hash[:username]
+      raise user_hash[:username].inspect
       user=User.find_or_create_by(username:user_hash[:username])
       self.user_id=user.id
     end
