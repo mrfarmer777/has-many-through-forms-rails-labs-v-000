ef0547d4c84847861b2d1c9dca08a5f12a582b9f
Matt Farmer <mrfarmer777@gmail.com>
2018-07-17 04:07:53 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   def create
     #raise params.inspect
     comment = Comment.create(comment_params)
-
+    raise comment.user.inpsect
     redirect_to post_path(comment.post)
   end
 
