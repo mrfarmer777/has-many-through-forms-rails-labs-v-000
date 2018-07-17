247a923710e18b89ddd6f1fbdc6f050acb0b1c3b
Matt Farmer <mrfarmer777@gmail.com>
2018-07-17 04:37:31 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: !:new_record?
+  #accepts_nested_attributes_for :user, reject_if: !:new_record?
 
 end
