44c78b0c2127336e8a2c531204e78042fbf76750
Matt Farmer <mrfarmer777@gmail.com>
2018-07-17 04:44:37 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     redirect_to post_path(comment.post)
   end
 
-  def users_attributes=(user_hash)
+  def user_attributes=(user_hash)
     raise user_hash.inspect
     if user_hash['username'].present?
       user=User.find_or_create_by(username:user_hash[:username])
@@ -19,6 +19,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, :users_attributes=>[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, :user_attributes=>[:username])
   end
 end
